f93ce5f68e0dabf5b91269cf07e4aed45907e415
ivaylopa <ivaylopa@buffalo.edu>
2016-04-27 16:35:02 -0400

    Newest control_box code
    Added RS232 code on Serial3

--- Control_box/control_box/control_box.ino
+++ Control_box/control_box/control_box.ino
@@ -11,7 +11,6 @@ struct RECEIVE_DATA_STRUCTURE{
   char chardata[4];
 };
 
-
 SEND_DATA_STRUCTURE dataSend;
 RECEIVE_DATA_STRUCTURE dataReceive;
 
@@ -33,19 +32,15 @@ RECEIVE_DATA_STRUCTURE dataReceive;
 #define MAX_ANGLE 80
 #define MIN_ANGLE 10
 #define MAX_PRESSURE 50
-#define MIN_PRESSURE 10
+#define MIN_PRESSURE -1
 
 int joystickupheld = 0;
 int joystickdwheld = 0;
 int fired = 0;
 int ignoreFlag = 0;					// flag is set if the cannon is locked. Ignore all interrupt input.
-int angleLow = 0;					// flags to ignore input on conditions.
-int angleHigh = 0;
-int pressureLow = 0;
-int pressureHigh = 0;
 int CannonReadyFlag = 0;			// Is the Cannon ready to accept commands
 int safe = 1;						// mode of the cannon for now only 1 or 0
-int angle = -1;						// cannon angle. Initialized to -1 so it doesn't display at the start.
+int angle = 30;						// cannon angle. Initialized to -1 so it doesn't display at the start.
 int pressure = 0;					// cannon pressure. Same as above.
 char* command;						// pointer to the string we will get over serial and interpreted as a command
 long debouncing_time = 100;			// I think it's in milisec. If interrupt is triggering more than once increase this value
@@ -59,16 +54,18 @@ void setup()
   pinMode(BL, OUTPUT);
   digitalWrite(LL, HIGH);
   digitalWrite(BL, HIGH);
-  lcd.begin(20, 4);                         // 20, 4 is our lcd size
+  lcd.begin(20, 4);// 20, 4 is our lcd size
 	displayMenu();														// Initialize the lcd to display the Menu without values
+//  lcd.clear();
 	pinMode(AUP, INPUT_PULLUP);											// Making the interrupt pin INPUT_PULLUP makes it less sensitive to noise, but switches the logic. 
 	pinMode(ADW, INPUT_PULLUP);											// Making the interrupt pin INPUT_PULLUP makes it less sensitive to noise, but switches the logic.
 	pinMode(PUP, INPUT_PULLUP);											// Making the interrupt pin INPUT_PULLUP makes it less sensitive to noise, but switches the logic.
 	pinMode(PDW, INPUT_PULLUP);											// Making the interrupt pin INPUT_PULLUP makes it less sensitive to noise, but switches the logic.
 	pinMode(ARM, INPUT_PULLUP);											// Making the interrupt pin INPUT_PULLUP makes it less sensitive to noise, but switches the logic.
 	pinMode(FIR, INPUT_PULLUP);											// Making the interrupt pin INPUT_PULLUP makes it less sensitive to noise, but switches the logic.
-	Serial.begin(19200);
-  Serial2.begin(19200);
+	Serial.begin(9600);
+  Serial2.begin(9600);                            // Direct EasyTransfer UART
+  Serial3.begin(9600);                            // BLIP RS232 UART
   ETout.begin(details(dataSend), &Serial2);
   ETin.begin(details(dataReceive), &Serial2);
   dataSend.chardata[0] = 'f';
@@ -76,6 +73,7 @@ void setup()
   dataSend.chardata[2] = '0';
   dataSend.chardata[3] = 0;
   ETout.sendData();
+  Serial3.print(dataSend.chardata);
 }
 
 void loop() 
@@ -87,7 +85,7 @@ void loop()
     TCCR1B = 0;
     TCNT1  = 0;               // Reset the count in the timer
 
-    OCR1A = 10000;            // compare match register I'm not sure exactly how much that value ammounts to but it is roughly 200-250 milisec
+    OCR1A = 5500;            // compare match register I'm not sure exactly how much that value ammounts to but it is roughly 200-250 milisec
     TCCR1B |= (1 << WGM12);   // Clear Timer Count on interrupt mode
     TCCR1B |= (1 << CS12);    // 256 prescaler means that the timer wil tick 256 times slower than the clock speed
     TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
@@ -100,64 +98,24 @@ void loop()
 
   if(ETin.receiveData())
   {
-    displayMenu();
-    updateMenu();
     Serial.print(String(dataReceive.chardata));
-    switch(dataReceive.chardata[0])        // If it is a then the command is aXX where XX is the curent angle. Example: a17 is angle of 17 deg.
-    {
-      case 'a':   //angle
-      {
-        if(angle != (dataReceive.chardata[1] - 0x30)*10 + (dataReceive.chardata[2] - 0x30))
-        {
-          angle = (dataReceive.chardata[1] - 0x30)*10 + (dataReceive.chardata[2] - 0x30);   // I don't know if this is the best way to convert the 2nd and 3rd char to a number but it works.
-          updateMenu();
-        }
-        break;
-      }
-      case 'p':   //pressure
-      {
-        pressure = (dataReceive.chardata[1] - 0x30)*10 + (dataReceive.chardata[2] - 0x30);
-        updateMenu();
-        break;
-      }
-      case 'f':   //firecode (error code)
-      {
-        switch(dataReceive.chardata[1])
-        {
-          case '0':     // Error State
-          {
-            if(dataReceive.chardata[2] == '1')
-            {
-              ignoreFlag = !ignoreFlag;
-              if(ignoreFlag)
-              {
-                lcd.clear();
-                lcd.print("Cannon Error");
-              }
-              else
-              {
-                displayMenu();
-                updateMenu();
-              }
-              break;
-            }
-            break;
-          }
-          case '3':     // Cannon is ready.
-          {
-            CannonReadyFlag = 1;
-            break;
-          }
-          case '4':     // Flip Cannon State
-          {
-            arm();
-            break;
-          }
-        }
-        break;
-      }
-    }
+    handleCommand(dataReceive.chardata);
   }
+}
+
+void serial3Event()
+{
+  char buf[128] = "";          // Set up a small buffer for readBytes
+  
+  if(Serial3.available() < 4)
+    return;                    // do nothing until we have 4 characters(1 command)
+
+  Serial3.readBytes(buf, 4);     // Read the command
+
+  Serial.print(buf);
+  
+  handleCommand(buf);           
+  
 }
 
 ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
@@ -173,7 +131,19 @@ ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
   if(!digitalRead(AUP))
   {
     if(angle < MAX_ANGLE)
+    {
       angle++;
+      if(angle%1 == 0)                        //Send the command every X increases of the angle. Currently 1.
+      {
+        dataSend.chardata[0] = 'a';
+        dataSend.chardata[1] = angle/10 + 0x30;
+        dataSend.chardata[2] = angle%10 + 0x30;
+        dataSend.chardata[3] = 0;
+        ETout.sendData();
+        Serial3.print(dataSend.chardata);
+        Serial3.write('a');
+      }
+    }
     updateMenu();
     joystickupheld = 1;
   }
@@ -184,12 +154,24 @@ ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
     dataSend.chardata[2] = angle%10 + 0x30;
     dataSend.chardata[3] = 0;
     ETout.sendData();
+    Serial3.print(dataSend.chardata);
     joystickupheld =0;
   }
   if(!digitalRead(ADW))
   {
     if(angle > MIN_ANGLE)
+    {
       angle--;
+      if(angle%1 == 0)
+      {
+        dataSend.chardata[0] = 'a';
+        dataSend.chardata[1] = angle/10 + 0x30;
+        dataSend.chardata[2] = angle%10 + 0x30;
+        dataSend.chardata[3] = 0;
+        ETout.sendData();
+        Serial3.print(dataSend.chardata);
+      }
+    }
     updateMenu();
     joystickdwheld = 1;
   }
@@ -200,6 +182,7 @@ ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
     dataSend.chardata[2] = angle%10 + 0x30;
     dataSend.chardata[3] = 0;
     ETout.sendData();
+    Serial3.print(dataSend.chardata);
     joystickdwheld =0;
   }
   if(!digitalRead(PUP))
@@ -232,6 +215,7 @@ void pressureUp()
     dataSend.chardata[3] = 0;
     Serial.print(String(dataSend.chardata));
     ETout.sendData();
+    Serial3.print(dataSend.chardata);
     updateMenu();
 }
 
@@ -244,6 +228,7 @@ void pressureDown()
     dataSend.chardata[3] = 0;
     Serial.print(String(dataSend.chardata));
     ETout.sendData();
+    Serial3.print(dataSend.chardata);
     updateMenu();
 }
 
@@ -256,6 +241,7 @@ void arm()
   dataSend.chardata[3] = 0;
   Serial.print(String(dataSend.chardata));
   ETout.sendData();
+  Serial3.print(dataSend.chardata);
 	safe = !safe;
   digitalWrite(BL, safe);
   fired = 0;
@@ -273,18 +259,19 @@ void fire()
     dataSend.chardata[3] = 0;
     Serial.print(String(dataSend.chardata));
     ETout.sendData();
+    Serial3.print(dataSend.chardata);
   }
 }
 
 void displayMenu()
 {
 	lcd.clear();
-	lcd.setCursor(0, 0);			// Sets the lcd cursor to 1st line beggining
-	lcd.print("Cannon state:");
-	lcd.setCursor(0, 1);			// 2nd line beginning and etc.
-	lcd.print("Angle:");
-	lcd.setCursor(0, 2);
-	lcd.print("Pressure:");
+	lcd.setCursor(0, 1);			// Sets the lcd cursor to 1st line beggining
+	lcd.print("deg");
+	lcd.setCursor(8, 1);			// 2nd line beginning and etc.
+	lcd.print("PSI");
+	lcd.setCursor(16, 1);
+	lcd.print("ft/s");
 //	lcd.setCursor(0, 3);
 //	lcd.print("Speed:");
 }
@@ -293,14 +280,76 @@ void displayMenu()
 // 7 will show up as 07
 void updateMenu()
 {
-	lcd.setCursor(15, 0);
-	lcd.print(String((safe)?" SAFE":"ARMED"));			//Depending on the state variable choose between the 2 strings around the : character
-	if(angle > 0)
+	if(angle >= 0)
 	{
-		lcd.setCursor(17, 1);
-		lcd.print(String(angle) + char(223));              	//decmal value 223 is the value for the degree sign for the J204A degree character.
+		lcd.setCursor(0, 0);
+		lcd.print(String(angle));              	//decmal value 223 is the value for the degree sign for the J204A degree character.
 	}
-  lcd.setCursor(17, 2);
-	lcd.print(String((pressure)?" ON":"OFF"));
+  if(pressure >= 0)
+  {
+    lcd.setCursor(9, 0);
+	  lcd.print(String(pressure));
+    lcd.setCursor(17, 0);
+    lcd.print(String(pressure/3));
+  }
+}
+
+
+void handleCommand(char* command)
+{
+  switch(command[0])        // If it is a then the command is aXX where XX is the curent angle. Example: a17 is angle of 17 deg.
+  {
+    case 'a':   //angle
+    {
+      if(angle != (command[1] - 0x30)*10 + (command[2] - 0x30))
+      {
+        angle = (command[1] - 0x30)*10 + (command[2] - 0x30);   // I don't know if this is the best way to convert the 2nd and 3rd char to a number but it works.
+        updateMenu();
+      }
+      break;
+    }
+    case 'p':   //pressure
+    {
+      pressure = (command[1] - 0x30)*10 + (command[2] - 0x30);
+      updateMenu();
+      break;
+    }
+    case 'f':   //firecode (error code)
+    {
+      switch(command[1])
+      {
+        case '0':     // Error State
+        {
+          if(command[2] == '1')
+          {
+            ignoreFlag = !ignoreFlag;
+            if(ignoreFlag)
+            {
+              lcd.clear();
+              lcd.print("Cannon Error");
+            }
+            else
+            {
+              displayMenu();
+              updateMenu();
+            }
+            break;
+          }
+          break;
+        }
+        case '3':     // Cannon is ready.
+        {
+          CannonReadyFlag = 1;
+          break;
+        }
+        case '4':     // Flip Cannon State
+        {
+          arm();
+          break;
+        }
+      }
+      break;
+    }
+  }
 }
 
